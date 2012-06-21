1856557f2e9c6b33c5feff43a6842629807d7b0c
Felix Stiehler <mail@felix-stiehler.de>
2012-06-21 04:09:51 +0200

    homepage stuff half done

--- main/views.py
+++ main/views.py
@@ -10,13 +10,22 @@ from django.contrib.sites.models import Site
 from streams.main.models import (Stream, Interval, StreamNumber,
     StreamNumberType, StreamType)
 
+def dictfetchall(cursor):
+    "Returns all rows from a cursor as a dict"
+    desc = cursor.description
+    return [
+        dict(zip([col[0] for col in desc], row))
+        for row in cursor.fetchall()
+    ]
+
 def homepage(request):
     latest_interval = Interval.objects.order_by('-date')[0]
     current_site = Site.objects.get_current()
     stream_numbers= StreamNumber.objects.select_related() \
+        .filter(stream_type__name='SC2') \
         .filter(interval=latest_interval) \
         .filter(stream_number_type__id=1) \
-        .order_by('-number')
+        .order_by('-number')[:12]
     return render_to_response('homepage.html',
                               {
                                 'stream_numbers': stream_numbers,
@@ -24,6 +33,24 @@ def homepage(request):
                                 'current_site': current_site,
                               },
                               context_instance=RequestContext(request))
+
+def search(request, query):
+    from django.db import connection, transaction
+    latest_interval = Interval.objects.order_by('-date')[0]
+
+    cursor = connection.cursor()
+    cursor.execute('SELECT s.name, s.id, sn.number FROM main_stream as s \
+                    LEFT JOIN main_streamnumber as sn \
+                    ON sn.stream_id = s.id and sn.interval_id = %s \
+                    WHERE s.name ILIKE \'\%%s\%\' \
+                    ORDER BY \
+                        CASE WHEN sn.number IS NULL THEN -1 \
+                        ELSE sn.number END DESC;'
+                    [
+                        latest_interval.id,
+                        query
+                    ]);
+    return HttpResponse(json.dumps(dictfetchall(cursor))
 
 def detail(request, stream_id):
     try:
@@ -38,16 +65,8 @@ def detail(request, stream_id):
                               },
                               context_instance=RequestContext(request))
 
-def stream_numbers(request, stream_id, time_span):
-    def dictfetchall(cursor):
-        "Returns all rows from a cursor as a dict"
-        desc = cursor.description
-        return [
-            dict(zip([col[0] for col in desc], row))
-            for row in cursor.fetchall()
-        ]
-
-    def get_stream_numbers(stream, min_time, snt_name):
+def stream_numbers(request, stream_id, time_span, time_span_end='latest'):
+    def get_stream_numbers(stream, min_time, max_time, snt_name):
         from django.db import connection, transaction
 
         cursor = connection.cursor()
@@ -58,12 +77,14 @@ def stream_numbers(request, stream_id, time_span):
                                 AND sn.stream_id = %s  \
                                 AND sn.stream_number_type_id = %s) \
                         WHERE i.date >= %s \
+                            AND i.date <= %s \
                             AND i.stream_number_type_id >= %s \
                         ORDER BY i.date ASC',
                         [
                             stream.id,
                             snt.id,
                             min_time,
+                            max_time,
                             snt.id,
                         ]);
 
@@ -81,21 +102,19 @@ def stream_numbers(request, stream_id, time_span):
     except Stream.DoesNotExist:
         return HttpResponse('Invalid stream id')
 
-    # now = datetime.datetime(2012, 6, 16, 02, 15, 02)
-    now = datetime.datetime.now()
-    if time_span == 'hour':
-        min_time = now - datetime.timedelta(hours=1, minutes=5)
-    elif time_span == 'day':
-        min_time = now - datetime.timedelta(days=1, minutes=30)
-    elif time_span == 'week':
-        min_time = now - datetime.timedelta(days=7, hours=4)
-    elif time_span == 'month':
-        min_time = now - datetime.timedelta(days=30)
-    elif time_span == 'year':
-        min_time = now - datetime.timedelta(years=1)
-    elif time_span == 'forever':
-        # there is no data from 2000 or before
-        min_time = datetime.datetime(2000, 1, 1, 0, 0, 0)
+    if time_span_end == 'latest':
+        max_time = datetime.datetime.now()
+    else:
+        max_time = datetime.datetime.fromtimestamp(int(time_span_end) + 1)
 
-    return_data = get_stream_numbers(stream, min_time, time_span)
+    if time_span == 'hour':
+        min_time = max_time - datetime.timedelta(hours=1, minutes=5)
+    elif time_span == 'day':
+        min_time = max_time - datetime.timedelta(days=1, minutes=30)
+    elif time_span == 'week':
+        min_time = max_time - datetime.timedelta(days=7, hours=4)
+    elif time_span == 'month':
+        min_time = max_time - datetime.timedelta(days=30)
+
+    return_data = get_stream_numbers(stream, min_time, max_time, time_span)
     return HttpResponse(json.dumps(return_data))
